79e4d96ded43077e1f71423bc5476afaa55c7d4c
Jack Kay <JKay@rcu.local>
2019-08-07 16:52:08 +0100

    Version 1.1

--- Rcu tool using V4 -With GUI .py
+++ Rcu tool using V4 -With GUI .py
@@ -2,18 +2,25 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from bs4 import BeautifulSoup
+from tkinter.filedialog import askdirectory
+import tkinter.messagebox
 
 #Create window interface / Labels 
 root = Tk()
+root.iconbitmap(r'C:\Users\jkay\AppData\Local\Programs\Python\Python37-32\DLLs\RCU2.ico')
 root.title("RCU Anonymiser Tool")
+from PIL import Image, ImageTk
 
 #Triggering the anonamising
+
+
 def OpenFile(): #todo add vaildation
-    name = askopenfilename(initialdir="C:/Users/Batman/Documents/Programming/tkinter",
-                           filetypes =(("XML File", "*.XML"),("All Files","*.*")),
-                           title = "Choose the ILR file."
-                           )
-    print (name)
+    name = askopenfilename(filetypes=(("XML File", "*.XML"), ("All Files", "*.*")),
+                           title="Choose the ILR file.")
+
+    outputlocation =askdirectory(title = "Select Output Location.")
+
+
     #Using try in case user types in unknown file or closes without choosing a file.
     try:
         with open(name,'r') as infile:
@@ -61,19 +68,12 @@ def OpenFile(): #todo add vaildation
         for NiNumber_tag in soup.find_all("NINumber"):
             NiNumber_tag.string = "NULL"
 
-        #Postcodes (Deleting last 3 digits)#
-        for pripostcode_tag in soup.find_all("PostcodePrior"):   
-            pripostcode_tag.string = pripostcode_tag.string[:-3]
-   
-        for postcode_tag in soup.find_all("Postcode"):   
-            postcode_tag.string = postcode_tag.string[:-3]
-
         # Find and Add three tags for
         # Deprivation Band
         # Local Authority District
-        # Local Authoirty Ward 
+        # Local Authoirty Ward
         for item in soup.select('Learner'):
-            finaltag=item.select('LearningDelivery')[-1]
+            finaltag = item.select('LearningDelivery')[-1]
             new_tag = soup.new_tag("law-ru")
             new_tag.append("NULL")
             finaltag.insert_after(new_tag)
@@ -84,19 +84,40 @@ def OpenFile(): #todo add vaildation
             new_tag.append("NULL")
             finaltag.insert_after(new_tag)
 
+
+
+        #Add postcode #todo start adding postcode matches to the above fields.
+
+
+
+        #Postcodes (Deleting last 3 digits)#
+        for pripostcode_tag in soup.find_all("PostcodePrior"):   
+            pripostcode_tag.string = pripostcode_tag.string[:-3]
+   
+        for postcode_tag in soup.find_all("Postcode"):   
+            postcode_tag.string = postcode_tag.string[:-3]
+
+
+
         # Output And Save Modified File
 
-        with open("SEND_ME_TO_RCU2.xml", "w") as outfile:
+        with open(outputlocation + "\RCU_Anon.xml", "w") as outfile:
             outfile.write(soup.prettify())
+
+        tkinter.messagebox.showinfo("Finished Anonymisation!", "Thank you. The file will be located\nin your selected output location") #Make this appear in a widget.
     except:
-         print("No file exists")
+         tkinter.messagebox.showinfo("Failed Anonymisation!", "Anonymisation Failed! Please retry or contact RCU \n  Tel: 01772 734855  |  Email: Mides@rcu.co.uk")
 
 #Programme View
-root.geometry ("300x100")
-OverviewLabel = Label(root, text="Please insert the ILR file below (XML Format)")
-OverviewLabel2 = Label(root, text="I am going to explain how to use the tool")
+root.geometry ("500x350")
+OverviewLabel3 = Label(root, text="Tel: 01772 734855  |  Email: Mides@rcu.co.uk", bd=1, relief=SUNKEN,)
+OverviewLabel = Label(root, text='\n You will be asked for your ILR first then asked to give a output location for the produced ILR \n This file will be called "RCU_Anon_ORIGFILENAME"')
+OverviewLabel2 = Label(root, text=" \nPlease insert the ILR file below (XML Format) to start the process.")
+img = Image.open("RCULOGO.png")
+rculogo = ImageTk.PhotoImage(img)
+Label(root, image=rculogo).pack()
 
-#Top Frame 
+#Top Frame
 topFrame = Frame(root)
 topFrame.pack(side=TOP)
 
@@ -105,11 +126,13 @@ bottomFrame = Frame(root)
 bottomFrame.pack(side=BOTTOM)
 
 #Widgits (Buttons)
-Button1 = Button(bottomFrame, text="    Select ILR File   ", fg="Black", bg="Yellow", command=OpenFile)
+Button1 = Button(bottomFrame,  text="    Being Anonymisation   ", fg="Black", bg="Yellow", command=OpenFile)
 Button1.pack(side=LEFT)
 
+OverviewLabel3.pack()
 OverviewLabel.pack()
 OverviewLabel2.pack()
+
 
 #Start Event 
 root.mainloop()
