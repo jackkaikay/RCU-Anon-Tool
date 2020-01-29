729e6bebcf8b01b3cc4577ef18be4bbffe8e2807
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-01-29 14:24:50 +0000

    Working product
    29/01/2020  14:24

--- RCUTool.py
+++ RCUTool.py
@@ -267,6 +267,7 @@ def restart_program():
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
+
 if __name__ == '__main__':
     root = Tk()
     root.configure(background='black')
